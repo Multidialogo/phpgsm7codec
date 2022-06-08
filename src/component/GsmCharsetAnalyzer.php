1c32915a8cc9a795ef237a5e1863dff5ec2c0e1b
ZioMitch <carino@netbuilder.it>
2022-06-08 07:35:10 +0200

    Refactor classes to be more dry

---
+++ src/component/GsmCharsetAnalyzer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace multidialogo\phpgsm7codec\component;
+
+class GsmCharsetAnalyzer
+{
+    private array $alphabet;
+
+    public function __construct(string $protocolVersion)
+    {
+        $this->alphabet = require_once (__DIR__ . "/alphabet/{$protocolVersion}.php");
+    }
+
+    public function countExtraCodecChars(string $input): int
+    {
+        $len = mb_strlen($input, 'UTF-8');
+        $count = 0;
+
+        for ($i = 0; $i < $len; $i++) {
+            if (!in_array(mb_substr($input, $i, 1, 'UTF-8'), $this->alphabet)) {
+                $count++;
+            }
+        }
+
+        return $count;
+    }
+}
--- src/tests/component/GsmCharsetAnalyzerTest.php
+++ src/tests/component/GsmCharsetAnalyzerTest.php
@@ -3,27 +3,31 @@
 namespace multidialogo\phpgsm7codec\tests\component;
 
 use PHPUnit\Framework\TestCase;
-use multidialogo\phpgsm7codec\component\CharsetAnalyzer;
+use multidialogo\phpgsm7codec\component\GsmCharsetAnalyzer;
 
-class CharsetAnalyzerTest extends TestCase
+class GsmCharsetAnalyzerTest extends TestCase
 {
     /**
      * @dataProvider provideInputWithExtraCodecChars
      */
-    public function testCountExtraCodecChars(string $input, int $extraCodecCount): void
+    public function testCountExtraCodecChars(string $protocolVersion, string $input, int $extraCodecCount): void
     {
-        static::assertEquals($extraCodecCount, CharsetAnalyzer::countExtraCodecChars($input));
+        $analyzer = new GsmCharsetAnalyzer($protocolVersion);
+
+        static::assertEquals($extraCodecCount, $analyzer->countExtraCodecChars($input));
     }
 
     public function provideInputWithExtraCodecChars(): array
     {
         return [
             [
+                '03.38',
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " .
                 "et dolore magna aliqua.',
                 0,
             ],
             [
+                '03.38',
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " .
                 "et dolore magna aliqua°',
                 1,
